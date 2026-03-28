8535c5e5209541243dbc85c50ed097d9b5291bae
Nick Hettinger <nickhett707@gmail.com>
2026-03-27 23:20:35 -0500

    revert.... kms

--- php/save_process_documents.php
+++
@@ -1,9 +0,0 @@
-<?php
-// save_json.php
-$json = file_get_contents('php://input');
-if (file_put_contents('../json/process_documents.json', $json)) {
-    echo json_encode(['status' => 'success']);
-} else {
-    echo json_encode(['status' => 'error', 'message' => 'Failed to write file']);
-}
-?>
